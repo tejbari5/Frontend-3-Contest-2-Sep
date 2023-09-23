98d1e53c151979560402fb1a8bde6f892adadee0
tejbari5 <136843098+tejbari5@users.noreply.github.com>
2023-09-23 21:39:50 +0530

    first commit

--- script.js
+++ script.js
@@ -1,90 +1,45 @@
-const pageName = window.location.pathname;
-const isSignUpPage = pageName == '/signup-ui' || pageName == '/signup-ui/' || pageName == '/signup-ui/index.html';
+// Signup Page
+const form = document.getElementById("signupForm");
+const error = document.getElementById("errorMessage");
 
-/**
- * Check user token
- */
-const userInfo = window.localStorage.getItem("--USER-INFO--");
-const message =  window.localStorage.getItem("--USER-MESSAGE");
-
-if (userInfo && isSignUpPage) {
-    window.location.href = `https://tejbari5.github.io/signup-ui/profile.html`
-} else if (!userInfo && !isSignUpPage) {
-    window.location.href = `https://tejbari5.github.io/signup-ui/`
+function generateAccessToken() {
+    let characters = '0123456789abcde';
+    let token = '';
+    for (let i = 0; i < 16; i++) {
+        token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
 }
 
-if (message) {
-    // if has message
-    document.querySelector(".alert").textContent = message;
-    window.localStorage.removeItem("--USER-MESSAGE");
+// Redirect to profile page if a logged-in user tries to navigate to the signup page
+const userJSON = localStorage.getItem('user');
+if (userJSON && window.location.href.includes('signup.html')) {
+    window.location.href = 'profile.html';
+}else{
+    form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmpassword').value;
+    if (password === confirmPassword) {
+        const accessToken = generateAccessToken();
+        const user = { username, email, password, accessToken };
+        localStorage.setItem('user', JSON.stringify(user));
+        error.textContent = 'Signup successful. Redirecting...';
+        setTimeout(() => {
+            window.location.href = 'profile.html';
+        }, 1000);
+    } else {
+        error.textContent = 'Please provide correct password.';
+    }
+});
+
 }
 
-// if it is profile page
-if (!isSignUpPage) {
-    const info = JSON.parse(userInfo);
 
-    document.querySelector("ul").innerHTML = `
-        <li>
-            <b>Full Name:</b>
-            <span>${info.name}</span>
-        </li>
-        <li>
-            <b>Email:</b>
-            <span>${info.email}</span>
-        </li>
-        <li>
-            <b>Token:</b>
-            <span>${info.accessToken}</span>
-        </li>
-        <li>
-            <b>Password:</b>
-            <span>${info.pass}</span>
-        </li>
-    `
-}
 
-document.querySelector("form").onsubmit = (e) => {
-    e.preventDefault();
 
-    // If page is `profile.html`
-    if (!isSignUpPage) {
-        window.localStorage.removeItem("--USER-INFO--")
-        window.localStorage.setItem("--USER-MESSAGE", "Logout successfully!");
-        window.location.href = `https://tejbari5.github.io/signup-ui/`
-        return
-    }
+  
 
-    const name = document.querySelector("#name").value;
-    const email = document.querySelector("#email").value;
-    const pass = document.querySelector("#password").value;
-    const cpass = document.querySelector("#cpassword").value;
-
-    if (!name || !email || !pass || !cpass) {
-        document.querySelector(".form-alert").textContent = `Error: All fields are mandatory!`
-        return
-    } else if (pass != cpass) {
-        document.querySelector(".form-alert").textContent = `Error: Password & Confirm Password does not match.`
-        return
-    }
-
-    let accessToken = [];
-    {
-        const string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for(let i = 0; i < 16; i++) {
-            accessToken.push(string[Math.floor(Math.random() * 62)]);
-        }
-    }
-
-    console.log("scsd")
-
-    const USER = {
-        name,
-        email,
-        pass,
-        accessToken: accessToken.join("")
-    }
-
-    window.localStorage.setItem("--USER-INFO--", JSON.stringify(USER));
-    window.localStorage.setItem("--USER-MESSAGE", "Signup successfully!");
-    window.location.href = `https://tejbari5.github.io/signup-ui/profile.html`;
-}
+ 
